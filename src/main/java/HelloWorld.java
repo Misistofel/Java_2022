3e158a6c9d3784d0e964befc5b2ac819635bb40f
margo <marinatofel@gmail.com>
2022-01-16 15:52:09 +0200

    Added " System.out.println("Try to compare git pull and git fetch");

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -2,6 +2,7 @@ public class HelloWorld {
   public static void main(String[] args) {
     System.out.println("Hello world!" );
     System.out.println("I want create new branch and pull it to my local repo" );
+    System.out.println("Try to compare git pull and git fetch");
     
     
   }
