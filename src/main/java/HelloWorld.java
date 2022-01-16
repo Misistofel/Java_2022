6ee33c99bec95b1ea7597166361092c27573685f
Misistofel <marinatofel@gmail.com>
2022-01-16 15:41:50 +0200

    Update HelloWorld.java
    Added new sout

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -1,5 +1,8 @@
 public class HelloWorld {
   public static void main(String[] args) {
     System.out.println("Hello world!" );
+    System.out.println("I want create new branch and pull it to my local repo" );
+    
+    
   }
 }
