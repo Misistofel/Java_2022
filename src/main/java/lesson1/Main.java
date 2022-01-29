d5f8261d902fcaf1b7584bf874a8efe66c96abbe
margo <marinatofel@gmail.com>
2022-01-29 16:27:49 +0200

    Added WhileDo and FoeClass

---
+++ src/main/java/lesson1/Main.java
@@ -0,0 +1,16 @@
+package lesson1;
+
+public class Main {
+  public static void main(String[] args) {
+
+//    ConditionOperatorsMethods c = new ConditionOperatorsMethods();
+//    c.searchMax(3,6,9);
+//    c.searchMin(4,5,-3);
+//    c.searchEqual(2,3);
+//    c.mod(5, 15);
+
+    ForClass.showTime();
+
+
+  }
+}
