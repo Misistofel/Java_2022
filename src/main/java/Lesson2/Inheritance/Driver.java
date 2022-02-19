bf60ea3bf46fe9c5d8df7ed2117a46742f3af2b1
margo <marinatofel@gmail.com>
2022-02-19 16:19:49 +0200

    Created Inheritance package

---
+++ src/main/java/Lesson2/Inheritance/Driver.java
@@ -0,0 +1,7 @@
+package Lesson2.Inheritance;
+
+public class Driver extends Employee {
+  String carName;
+  String driverCategory;
+  int countOfVehicles;
+}
---
+++ src/main/java/Lesson2/Inheritance/Employee.java
@@ -0,0 +1,9 @@
+package Lesson2.Inheritance;
+
+public class Employee {
+  String name;
+  String lastName;
+  String nationality = "ukrainian";
+  String salary;
+  int holliday;
+}
